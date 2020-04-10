870f1570105d8b0261991615e7d52d17b7784b5b
mien <huangxiaomeng_vip@126.com>
2020-04-10 23:32:41 +0800

    add status tag

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const byId = (id) => document.getElementById(id);
 
+const updateStatus = status => byId('status').innerHTML = status;
+
+window.addEventListener('load', async () => {
+    updateStatus('stabled');
+});
 
 byId('reload').addEventListener('click', () => {
+    updateStatus('uploading');
     window.location.reload();
 })

47ab387a409513c23856487b3fa8781be71569a1
mien <huangxiaomeng_vip@126.com>
2020-11-02 11:43:11 +0800

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const byId = id => document.getElementById(id);
+const byId = (id) => document.getElementById(id);
 
-const byClassName = className => document.getElementsByClassName(className);
+const byClassName = (className) => document.getElementsByClassName(className);
 
-const updateStatus = status => byId('status-bar').style.display = status;
+const updateStatus = (status) => (byId('status-bar').style.display = status);
 let messageTimeoutId = 0;
 window.addEventListener('load', async () => {
     updateStatus('none');
@@ -12,7 +12,7 @@ window.addEventListener('load', async () => {
 byId('reload').addEventListener('click', () => {
     updateStatus('block');
     window.location.reload();
-})
+});
 
 byId('no-keyboard').addEventListener('focus', (event) => {
     console.log(event);
@@ -20,22 +20,45 @@ byId('no-keyboard').addEventListener('focus', (event) => {
     event.stopPropagation();
 });
 
+byId('download').addEventListener('click', () => {
+    const link = document.createElement('a');
+    //设置下载的文件名
+    link.download = 'test.png';
+    link.style.display = 'none';
+    //设置下载路径
+    var blob = new Blob(['12312312']);
+
+    //这里导出src，然后把这里的src赋给上面的src即可
+    var src = URL.createObjectURL(blob);
+    link.href = src;
+    //触发点击
+    document.body.appendChild(link);
+    link.click();
+    //移除节点
+    document.body.removeChild(link);
+});
+
+
+
 const mountItemClickEvent = () => {
     for (let item of byClassName('item')) {
         item.addEventListener('click', (evnet) => {
-            console.log(event)
+            console.log(event);
             clearTimeout(messageTimeoutId);
             byId('message').style.display = 'block';
             messageTimeoutId = setTimeout(() => {
                 byId('message').style.display = 'none';
             }, 3000);
-        })
+        });
     }
-}
+};
 
 mountItemClickEvent();
 
 const checkInternet = () => {
     const { onLine } = navigator;
-    byId('device').setAttribute('class', onLine ? 'iconfont icon-deviceon' : 'iconfont icon-deviceoff');
-}
+    byId('device').setAttribute(
+        'class',
+        onLine ? 'iconfont icon-deviceon' : 'iconfont icon-deviceoff'
+    );
+};
